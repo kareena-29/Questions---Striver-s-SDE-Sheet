94b70d81a404b20ec38b33e69fe8e4943bd7f8d8
Kareena Thakrel <kareenathakrel2002@gmail.com>
2023-06-04 23:39:07 +0530

    Next 2 problems

---
+++ Day_1/Count_Subarrays_with_XOR.java
@@ -0,0 +1,35 @@
+package Day_1;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Count_Subarrays_with_XOR {
+    public static int subarraysXor(ArrayList<Integer> arr, int k) {
+        int xr = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int cnt = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            xr = xr ^ arr.get(i);
+            int x = xr ^ k;
+            if (map.containsKey(x))
+                cnt += map.get(x);
+
+            map.put(xr, map.getOrDefault(xr, 0) + 1);
+        }
+        return cnt;
+    }
+
+
+    public static void main(String[] args) {
+        ArrayList<Integer> temp = new ArrayList<>();
+        temp.add(5);
+        temp.add(3);
+        temp.add(8);
+        temp.add(3);
+        temp.add(10);
+
+        System.out.println(subarraysXor(temp, 8));
+    }
+}
---
+++ Day_1/Longest_Substring_Without_Repeating.java
@@ -0,0 +1,27 @@
+package Day_1;
+
+import java.util.HashMap;
+
+public class Longest_Substring_Without_Repeating {
+    public static int uniqueSubstrings(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int left = 0, right = 0;
+        int n = s.length();
+        int len = 0;
+        while (right < n) {
+            if (map.containsKey(s.charAt(right))) {
+                left = Math.max(map.get(s.charAt(right)) + 1, left);
+
+            }
+            map.put(s.charAt(right), right);
+            len = Math.max(len, right - left + 1);
+            right++;
+        }
+        return len;
+    }
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+
+        System.out.println(uniqueSubstrings(s));
+    }
+}
